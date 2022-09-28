a5dea50a1de34cabd959c38c60ebe731e0e8182d
Arsen Shahinyan <102800732+ArsenSh@users.noreply.github.com>
2022-09-28 22:02:23 +0400

    Add files via upload

---
+++ 28-09-2022/memcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void* memcpy(void* dest, const void* src, size_t count) 
+{
+    size_t x = count;
+    while (count--) 
+    {
+        *(char*)dest = *(char*)src;
+        ++dest;
+        ++src;
+    }
+    return dest - x;
+}
+
+void main() 
+{
+    char buf1[] = "hello, i said hello";
+    char* buf2 = (char*)malloc(20);
+    buf2 = (char*)memcpy(buf2, buf1, 6);
+    printf("%s\n", buf2);
+}
